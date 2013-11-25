d9fcca71831ce9211a355a31b3b81dd889eb2448
R. Brent Calder <brent.calder@einstein.yu.edu>
2013-11-25 15:21:16 -0500

    cleaning

--- R/savR-package.R
+++ R/savR-package.R
@@ -1,23 +1,23 @@
-#' Parse and analyze Illumina SAV files
+#'Parse Illumina Sequence Analysis Viewer files 
 #' 
-#' \tabular{ll}{
-#' Package: \tab savR \cr
-#' Type: \tab Package \cr
-#' Version: \tab 0.99.0 \cr
-#' Date: \tab 2013-11-18 \cr
-#' License: \tab AGPL-3 \cr
-#' LazyLoad: \tab yes \cr
-#' }
+#'\tabular{ll}{
+#'Package: \tab savR \cr
+#'Type: \tab Package \cr
+#'Version: \tab 0.99.0 \cr
+#'Date: \tab 2013-11-25 \cr
+#'License: \tab AGPL-3 \cr
+#'LazyLoad: \tab yes \cr
+#'}
 #' 
-#' Parse Illumina Sequence Analysis Viewer files, provide
-#' visualizations and access to data.
+#'Parse Illumina Sequence Analysis Viewer files, provide
+#'visualizations and access to data.
 #' 
-#' @name savR-package
-#' @docType package
-#' @title Parse and analyze Illumina SAV files
-#' @author R. Brent Calder \email{brent.calder@@einstein.yu.edu}
-#' @references For information about Illumina SAV, please refer to \cr \url{http://supportres.illumina.com/documents/documentation/software_documentation/sav/sequencinganalysisviewer_userguide_15020619c.pdf} \cr For other implementations (and inspiration) please see \cr \url{http://search.cpan.org/dist/Bio-IlluminaSAV/Bio/IlluminaSAV.pm} \cr \url{https://bitbucket.org/invitae/illuminate}
-#' @keywords package
+#'@name savR-package
+#'@docType package
+#'@title Parse and analyze Illumina SAV files
+#'@author R. Brent Calder \email{brent.calder@@einstein.yu.edu}
+#'@references For information about Illumina SAV, please refer to \cr \url{http://supportres.illumina.com/documents/documentation/software_documentation/sav/sequencinganalysisviewer_userguide_15020619c.pdf} \cr For other implementations (and inspiration) please see \cr \url{http://search.cpan.org/dist/Bio-IlluminaSAV/Bio/IlluminaSAV.pm} \cr \url{https://bitbucket.org/invitae/illuminate}
+#'@keywords package
 #' 
 NULL
 
