2d60b79f03dcb2b8931c005fcc41c90ea0881c67
b.calder <b.calder@bc3139a8-67e5-0310-9ffc-ced21a209358>
2014-12-02 20:55:23 +0000

    Commit made by the Bioconductor Git-SVN bridge. Consists of 2 commits.
    Commit information:
    
    Commit id: 166ec390d4f918ffd60439a53ad032218dfc756f
    
        v1.5.2
    
    Committed by: R. Brent Calder
    Author Name: R. Brent Calder
    Commit date: 2014-12-02 15:54:19 -0500
    Author date: 2014-12-02 15:54:19 -0500
    
    Commit id: 05369fd6afcfa0542073050ca9bd9a88787b217c
    
        fixed bug with qualityHeatmap
    
    Committed by: R. Brent Calder
    Author Name: R. Brent Calder
    Commit date: 2014-12-02 15:53:43 -0500
    Author date: 2014-12-02 15:53:43 -0500
    
    
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/savR@97289 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/show-methods.R
+++ R/show-methods.R
@@ -1,4 +1,4 @@
 setMethod("show", "savProject", function(object) cat(class(object), "instance with", 
                                                      object@layout@lanecount, "lanes,", 
                                                      object@cycles, "total cycles and", 
-                                                     object@directions, "sequencing runs." ))
+                                                     object@directions, "sequencing runs.\n" ))
--- README.md
+++ README.md
@@ -4,7 +4,7 @@ savR
 *savR* is an R package to parse Illumina InterOp
 files for downstream analysis.
 
-Current release version 1.5.1.
+Current release version 1.5.2.
 
 Example
 --------
