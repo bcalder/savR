166ec390d4f918ffd60439a53ad032218dfc756f
R. Brent Calder <brent.calder@einstein.yu.edu>
2014-12-02 15:54:19 -0500

    v1.5.2

--- R/savR-package.R
+++ R/savR-package.R
@@ -3,7 +3,7 @@
 #'\tabular{ll}{
 #'Package: \tab savR \cr
 #'Type: \tab Package \cr
-#'Version: \tab 1.5.1 \cr
+#'Version: \tab 1.5.2 \cr
 #'Date: \tab 2014-11-12 \cr
 #'License: \tab AGPL-3 \cr
 #'LazyLoad: \tab yes \cr
