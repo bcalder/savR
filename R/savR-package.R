a8050c1be2cb92b8a8a199236b8621f40ef234ed
R. Brent Calder <robert.calder@mssm.edu>
2016-01-07 07:45:30 -0500

    remove scales import (ggplot2 now exports)

--- R/savR-package.R
+++ R/savR-package.R
@@ -14,7 +14,7 @@
 #' 
 #'@name savR-package
 #'@docType package
-#'@import methods ggplot2 reshape2 scales gridExtra XML
+#'@import methods ggplot2 reshape2 gridExtra XML
 #'@title Parse and analyze Illumina SAV files
 #'@author R. Brent Calder \email{brent.calder@@einstein.yu.edu}
 #'@references For information about Illumina SAV, please refer to \cr \url{http://supportres.illumina.com/documents/documentation/software_documentation/sav/sequencinganalysisviewer_userguide_15020619c.pdf} \cr For other implementations (and inspiration) please see \cr \url{http://search.cpan.org/dist/Bio-IlluminaSAV/Bio/IlluminaSAV.pm} \cr \url{https://bitbucket.org/invitae/illuminate}
